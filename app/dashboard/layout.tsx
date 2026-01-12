ad83ab379f21b2402b136101d1d5f60ae9d46e16
Richard Kane <rich@getvergo.com>
2026-01-12 10:06:22 -0500

    Force dynamic rendering for dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -5,6 +5,8 @@ import Link from "next/link"
 import React from "react"
 import { UserMenu } from "@/components/user-menu"
 
+export const dynamic = "force-dynamic"
+
 export default async function DashboardLayout({
   children,
 }: {
